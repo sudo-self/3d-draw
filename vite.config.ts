63e85922969a97b6e4f84e8be46571ddf42e8a20
sudo-self <tech@jessejesse.com>
2025-09-01 04:54:43 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: './', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 });
+
 
 
